c348e58b76148c3fc09f0c5656abf301830ca3b2
ejgardner_adrestia <ejgardner@adrestia.com>
2024-09-25 09:11:00 +0100

    Modified snp_list_generator.py to parse variant lists in parallel

--- collapsevariants/snp_list_generator.py
+++ collapsevariants/snp_list_generator.py
@@ -1,13 +1,22 @@
 import csv
 import dxpy
+import numpy as np
 import pandas as pd
 
+from enum import Enum, auto
 from pathlib import Path
-from typing import List
+from typing import Tuple
 
 from collapsevariants.ingest_data import IngestData
 from collapsevariants.collapse_logger import CollapseLOGGER
+from general_utilities.job_management.thread_utility import ThreadUtility
 from general_utilities.mrc_logger import MRCLogger
+
+
+class FilteringMode(Enum):
+    GENE_LIST = auto()
+    FILTERING_EXPRESSION = auto()
+    SNP_LIST = auto()
 
 
 class SNPListGenerator:
@@ -36,40 +45,83 @@ class SNPListGenerator:
 
         self._bgen_index = ingested_data.bgen_index
         self._filtering_expression = ingested_data.filtering_expression
-        self._found_snps = ingested_data.found_snps
-        self._found_genes = ingested_data.found_genes
+        self._found_genes = ingested_data.found_genes  # This is a boolean if we found a gene list
+        self._gene_list = None  # Placeholder for a numpy array of gene symbols
+        self._found_snps = ingested_data.found_snps  # This is a boolean if we found a SNP list
+        self._snp_list = None  # Placeholder for a numpy array of SNP IDs
 
         self._logger = MRCLogger(__name__).get_logger()
         self.log_file = log_file
 
-        self.variant_index = self._query_variant_index()
-        self.chromosomes = self._write_variant_lists()
+        # Decide on the type of filtering we are doing (SNP, Gene, or expression)
+        self._filtering_mode = self._decide_filtering_mode()
+
+        # Iterate through all possible bgens in parallel and filter them
+        self._variant_index = []
+        thread_utility = ThreadUtility()
+        for prefix in self._bgen_index.keys():
+            thread_utility.launch_job(self._query_variant_index,
+                                      prefix=prefix)
+
+        self.chromosomes = dict()
+        for thread in thread_utility:
+            current_table, prefix, chrom, vars_found = thread.result
+            if vars_found:
+                self._variant_index.append(thread.result)
+                if chrom not in self.chromosomes:
+                    self.chromosomes[chrom] = [prefix]
+                else:
+                    self.chromosomes[chrom].append(prefix)
+
+        self._variant_index = pd.concat(self._variant_index)
+
+        # Write the variant lists to be used for filtering
+        self.snp_enst_index_path, self.include_path = self._write_variant_lists()
+
+        # Check the stats of the bgen files
         self.total_sites = self._check_bgen_stats()
 
-    def _load_variant_index(self) -> pd.DataFrame:
-        """Load vep annotated *.tsv.gz files into a pandas DataFrame
+    def _decide_filtering_mode(self) -> FilteringMode:
 
-        Will iterate over all chromosomes that were found by collapse_variants.ingest_data, NOT the actual chromosomes
-        in the human genome (e.g., 1..23, X, Y).
+        if self._filtering_expression is not None and self._found_genes:
+
+            with Path('gene_list.genes').open('r') as my_genelist_file:
+                gene_list = my_genelist_file.readlines()
+                self._gene_list = np.array([gene.rstrip() for gene in gene_list])
+
+            return FilteringMode.FILTERING_EXPRESSION
+
+        # 2. Filtering expression
+        elif self._filtering_expression is not None:
+            return FilteringMode.FILTERING_EXPRESSION
+
+        # 3. SNP List
+        elif self._found_snps:
+
+            with Path('snp_list.snps').open('r') as my_snplist_file:
+                snp_list = my_snplist_file.readlines()
+                self._snp_list = np.array([snp.rstrip() for snp in snp_list])
+
+            return FilteringMode.SNP_LIST
+
+        else:
+            raise ValueError('No filtering expression, SNP list, or Gene list provided!')
+
+    @staticmethod
+    def _load_variant_index(prefix: str) -> pd.DataFrame:
+        """Load vep annotated *.tsv.gz files into a pandas DataFrame
 
         :return: A pandas.DataFrame containing variants loaded from all provided chromosomes
         """
 
-        variant_index = []
-        # Open all chromosome indicies and load them into a list
-        # Specify dtype for SIFT/POLYPHEN as pandas returns warnings when loading these due to weird formating
-        for chromosome in self._bgen_index.keys():
-            variant_index.append(
-                pd.read_csv(f'filtered_bgen/chr{chromosome}.filtered.vep.tsv.gz', sep='\t',
-                            dtype={'SIFT': str, 'POLYPHEN': str}))
-
-        variant_index = pd.concat(variant_index)
-        variant_index = variant_index.set_index('varID')
+        current_vep = pd.read_csv(f'{prefix}.filtered.vep.tsv.gz', sep='\t',
+                                  dtype={'SIFT': str, 'POLYPHEN': str})
+        variant_index = current_vep.set_index('varID')
 
         return variant_index
 
     # Actually run the query and modify the variant_index loaded above based on this/these queries
-    def _query_variant_index(self) -> pd.DataFrame:
+    def _query_variant_index(self, prefix: str) -> Tuple[pd.DataFrame, str, str, bool]:
         """Query self.variant_index based on the logic outlined in the __init__ method for this class
 
         As described in the __init__ for the class, there are three possibilities, each with an if/ifelse, and in this
@@ -79,26 +131,33 @@ class SNPListGenerator:
         2. Filtering Expression = Any variant which qualifies under the given filtering expression
         3. SNPList = Select specific SNPs to collapse on
 
-        :return: A pandas.DataFrame containing variants filtered based on provided input parameters
+        :param prefix: Prefix of the file to load. Handled  by :func:`_load_variant_index`
+        :return: A Tuple of four parts: 1) pandas.DataFrame containing variants from :param variant_index: filtered based on provided
+            input parameters 2) The prefix of the file loaded 3) The chromosome of all variants found 4) A boolean
+            indicating if any variants were found after filtering.
         """
 
-        # First load information for all variants into a pandas DataFrame
-        variant_index = self._load_variant_index()
+        variant_index = self._load_variant_index(prefix)
 
-        # Then query
         # 1. Filtering expression + Gene List
-        if self._filtering_expression is not None and self._found_genes:
+        if self._filtering_mode == FilteringMode.GENE_LIST:
             variant_index = self._query_gene_list(variant_index)
 
         # 2. Filtering expression
-        elif self._filtering_expression is not None:
+        elif self._filtering_mode == FilteringMode.FILTERING_EXPRESSION:
             variant_index = self._query_filtering_expression(variant_index)
 
         # 3. SNP List
-        elif self._found_snps:
+        elif self._filtering_mode == FilteringMode.SNP_LIST:
             variant_index = self._query_snp_list(variant_index)
 
-        return variant_index
+        poss_chrom = variant_index['CHROM'].unique()
+        if len(poss_chrom) > 1:
+            raise ValueError(f'More than one chromosome found in bgen {prefix}!')
+        else:
+            poss_chrom = poss_chrom[0]
+
+        return variant_index, prefix, poss_chrom, len(variant_index) > 0
 
     def _query_gene_list(self, variant_index: pd.DataFrame) -> pd.DataFrame:
         """Filter variants by a provided gene list
@@ -107,14 +166,8 @@ class SNPListGenerator:
         :return: A modified version of the variant_index pandas.DataFrame AFTER filtering on provided Gene list
         """
 
-        with Path('gene_list.genes').open('r') as my_genelist_file:
-            genelist = my_genelist_file.readlines()
-            genelist = [gene.rstrip() for gene in genelist]
-
         # First filter to the gene Symbols we care about
-        variant_index = variant_index.query('SYMBOL == @genelist')
-        all_genelist = list(variant_index['SYMBOL'].unique())
-        found_genes_symbol_present = list(set.intersection(set(genelist), set(all_genelist)))
+        variant_index = variant_index[variant_index['SYMBOL'].isin(self._gene_list)]
 
         # Now further filter down to the filtering_expression we are interested in
         variant_index = variant_index.query(self._filtering_expression)
@@ -122,21 +175,6 @@ class SNPListGenerator:
         # Set all gene ENSTs to ENST99999999999
         # This is a dummy value so that association_testing knows we are running a gene list
         variant_index['ENST'] = 'ENST99999999999'
-
-        # Following code is just to report stats about genes that were/were not found
-        queried_genes = []
-        self.log_file.write_header('Per-gene stats')
-        for key, value in variant_index['SYMBOL'].value_counts().items():
-            queried_genes.append(key)
-            self.log_file.write_int(key, value)
-        self.log_file.write_spacer()
-
-        for gene in genelist:
-            if gene not in found_genes_symbol_present:
-                self._logger.warning(f'Asked for gene – {gene} – gene symbol was not found in the variant index.')
-            elif gene not in queried_genes:
-                self._logger.warning(f'Asked for gene – {gene} – no variants in this gene after applying '
-                                     f'filtering expression.')
 
         return variant_index
 
@@ -160,34 +198,9 @@ class SNPListGenerator:
         :param variant_index: A pandas.DataFrame containing variants loaded from all provided chromosomes
         :return: A modified version of the variant_index pandas.DataFrame AFTER filtering on provided SNP list
         """
-        self.log_file.write_generic('Variants not Found:')
-        with Path('snp_list.snps').open('r') as snplist:
-            snps = []
-            not_found = []
-            for snp in snplist:
-                snp = snp.rstrip()
-                if snp in variant_index.index:
-                    snps.append(snp)
-                else:
-                    not_found.append(snp)
-                    self.log_file.write_generic(snp)
-            snplist.close()
-        self.log_file.write_spacer()
-
-        if len(snps) == 0:
-            raise dxpy.AppError('No SNPs remain after using SNPlist!')
-        else:
-            # Print to logfile variants that we found...
-            self.log_file.write_generic('Variants found')
-            for snp in snps:
-                self.log_file.write_generic(snp)
-            self.log_file.write_spacer()
-
-        self.log_file.write_int('Total variants found', len(snps))
-        self.log_file.write_spacer()
 
         # And finally extract variants here
-        variant_index = variant_index.loc[snps]
+        variant_index = variant_index.loc[self._snp_list]
 
         # Here we create a fake 'GENE' to collapse on later to make the process of generating a mask easier.
         # We also use this gene ID to notify runassociationtesting that we are doing a SNP-based mask
@@ -195,46 +208,34 @@ class SNPListGenerator:
 
         return variant_index
 
-    def _write_variant_lists(self) -> List[str]:
+    def _write_variant_lists(self) -> Tuple[Path, Path]:
         """Write the lists that will be used for filtering and generate a list of chromosomes that are valid for actual
             processing.
 
         Generate two files that will be used later:
 
-        1. A list of variants -> Genes
-        2. Just a list of variant IDs
+        1. A list of variants -> Genes (gene_ENST.txt)
+        2. Just a list of variant IDs (include_snps.txt)
 
         Also keep track of chromosomes that we found so we don't process chromosomes without a qualifying variant.
 
-        :return: A list of chromosomes that actually contain variants we collapsed on
+        :return: A Tuple consisting of two Path objects – one for the snp_enst_index (links snps to ENSTs) and one
+            for the include_path (just a list of snps).
         """
+
+        # Sort the variant index:
+        self._variant_index.sort_values(by=['CHROM', 'POS'], inplace=True)
 
         # Now prep a list of variants to include in any analysis. We need two files:
         # 1. Assigns a variant to a gene (gene_id_file)
         # 2. Just includes variant IDs (snp_id_file)
-        with Path('snp_ENST.txt').open('w') as gene_id_file,\
-                Path('include_snps.txt').open('w') as snp_id_file:
+        snp_enst_index_path = Path('gene_ENST.txt')
+        self._variant_index[['CHROM', 'POS', 'ENST']].to_csv(snp_enst_index_path, sep='\t', header=True, index=True)
 
-            gene_id_csv = csv.DictWriter(gene_id_file, delimiter='\t', fieldnames=['varID', 'chrom', 'pos', 'ENST'])
-            snp_id_csv = csv.DictWriter(snp_id_file, delimiter="\t", fieldnames=['varID'], extrasaction='ignore')
-            gene_id_csv.writeheader()
+        include_path = Path('include_snps.txt')
+        self._variant_index[[]].to_csv(include_path, sep='\t', header=False, index=True)  # Double bracket drops all cols
 
-            for row in self.variant_index.iterrows():
-                # row[0] in this context is the varID since it is the 'index' in the pandas DataFrame
-                # All other information is stored in a dictionary that is list element [1]
-                line_dict = {'varID': row[0],
-                             'chrom': row[1]['CHROM'].replace('chr', ''),
-                             'pos': row[1]['POS'],
-                             'ENST': row[1]['ENST']}
-                gene_id_csv.writerow(line_dict)
-                snp_id_csv.writerow(line_dict)
-
-        # Get chromosomes we want to actually run to save time downstream:
-        chromosomes = self.variant_index['CHROM'].value_counts()
-        chromosomes = chromosomes.index.to_list()
-        chromosomes = [str.replace(chrom, 'chr', '') for chrom in chromosomes]
-
-        return chromosomes
+        return snp_enst_index_path, include_path
 
     def _check_bgen_stats(self) -> int:
         """Gets information relating to included variants in bgen format files (per-gene)
@@ -246,35 +247,88 @@ class SNPListGenerator:
         """
 
         self.log_file.write_header('Overall Statistics')
-        total_sites = self.variant_index['CHROM'].count()  # Assign as a variable so I can return it below for further checking
+        total_sites = self._variant_index[
+            'CHROM'].count()  # Assign as a variable so I can return it below for further checking
         self.log_file.write_int('Total number of variants', total_sites)
-        self.log_file.write_float('Maximum missingness', self.variant_index['F_MISSING'].max() * 100)
-        self.log_file.write_scientific('Maximum Allele Frequency', self.variant_index['AF'].max(skipna=True))
-        self.log_file.write_float('Minimum CADD Score', self.variant_index['CADD'].min(skipna=True))
-        self.log_file.write_float('Minimum REVEL Score', self.variant_index['REVEL'].min(skipna=True))
+        self.log_file.write_float('Maximum missingness', self._variant_index['F_MISSING'].max() * 100)
+        self.log_file.write_scientific('Maximum Allele Frequency', self._variant_index['AF'].max(skipna=True))
+        self.log_file.write_float('Minimum CADD Score', self._variant_index['CADD'].min(skipna=True))
+        self.log_file.write_float('Minimum REVEL Score', self._variant_index['REVEL'].min(skipna=True))
         self.log_file.write_int('Number of NA REVEL Scores',
-                                self.variant_index[self.variant_index['REVEL'].isna()]['CHROM'].count())
+                                self._variant_index[self._variant_index['REVEL'].isna()]['CHROM'].count())
         self.log_file.write_int('Total number of PASS variants',
-                                self.variant_index[self.variant_index['FILTER'] == 'PASS']['CHROM'].count())
+                                self._variant_index[self._variant_index['FILTER'] == 'PASS']['CHROM'].count())
         self.log_file.write_int('Total number of non-PASS variants',
-                                self.variant_index[self.variant_index['FILTER'] != 'PASS']['CHROM'].count())
+                                self._variant_index[self._variant_index['FILTER'] != 'PASS']['CHROM'].count())
 
         # LOFTEE:
-        for key, value in self.variant_index['LOFTEE'].value_counts().items():
+        for key, value in self._variant_index['LOFTEE'].value_counts().items():
             key = f'Number of LOFTEE {key}'
             self.log_file.write_int(key, value)
         self.log_file.write_spacer()
 
         # Parsed Consequences:
         self.log_file.write_header('Consequence statistics')
-        for key, value in self.variant_index['PARSED_CSQ'].value_counts().items():
+        for key, value in self._variant_index['PARSED_CSQ'].value_counts().items():
             key = f'Number of Parsed Consequence – {key}'
             self.log_file.write_int(key, value)
 
         # VEP Consequences:
-        for key, value in self.variant_index['CSQ'].value_counts().items():
+        for key, value in self._variant_index['CSQ'].value_counts().items():
             key = f'Number of VEP Consequence - {key}'
             self.log_file.write_int(key, value, is_vep=True)
         self.log_file.write_spacer()
 
+        # Append filtering information on SNP / GENE lists if required:
+        if self._filtering_mode == FilteringMode.SNP_LIST or self._filtering_mode == FilteringMode.GENE_LIST:
+            self._check_list_filtering()
+
         return total_sites
+
+    def _check_list_filtering(self) -> None:
+        """This method will check against either the original SNP or GENE list to determine if variants / genes were
+        found in the final set of filtered variants
+        """
+
+        ## For SNP list
+        if self._filtering_mode == FilteringMode.SNP_LIST:
+
+            # Make a boolean array of SNPs that are in the final list
+            snp_boolean = np.isin(self._snp_list, self._variant_index.index)
+
+            # Then use the boolean to subset the snp array
+            missing_snps = self._snp_list[snp_boolean == False]
+            found_snps = self._snp_list[snp_boolean == True]
+
+            if len(found_snps) == 0:
+                raise dxpy.AppError('No SNPs remain after using SNPlist!')
+
+            else:
+
+                self.log_file.write_int('Total variants found', len(found_snps))
+                self.log_file.write_int('Total variants not found', len(missing_snps))
+                # Print to logfile variants that we found...
+                self.log_file.write_generic('Variants not Found:')
+                for snp in missing_snps:
+                    self.log_file.write_generic(snp)
+                self.log_file.write_spacer()
+
+        elif self._filtering_mode == FilteringMode.GENE_LIST:
+            ## For Gene list
+            # Following code is just to report stats about genes that were/were not found
+            all_genelist = self._variant_index['SYMBOL'].unique()  # np.ndarray
+            found_genes_symbol_present = all_genelist[np.isin(self._gene_list, all_genelist)]
+
+            queried_genes = []
+            self.log_file.write_header('Per-gene stats')
+            for key, value in self._variant_index['SYMBOL'].value_counts().items():
+                queried_genes.append(key)
+                self.log_file.write_int(key, value)
+            self.log_file.write_spacer()
+
+            for gene in self._gene_list:
+                if gene not in found_genes_symbol_present:
+                    self._logger.warning(f'Asked for gene – {gene} – gene symbol was not found in the variant index.')
+                elif gene not in queried_genes:
+                    self._logger.warning(f'Asked for gene – {gene} – no variants in this gene after applying '
+                                         f'filtering expression.')
