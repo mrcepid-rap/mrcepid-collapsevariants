05bcc998c7c53876d80d27b023a8a51920afbe57
Alish Palmos <alish.palmos@insmed.com>
2025-02-10 13:46:35 +0000

    revert saige parser back to foo

--- collapsevariants/tool_parsers/saige_parser.py
+++ collapsevariants/tool_parsers/saige_parser.py
@@ -75,10 +75,6 @@ class SAIGEParser(ToolParser):
                     raise ValueError('Genes are not sorted by position!')
 
                 prev_pos = gene_info['MIN']
-                output_setfile_SAIGE.write(f'{gene_info["ENST"]}\tvar\t{gene_info["VARS"]}\n')
-                # count the number of variants in gene_info["VARS"]
-                length_of_vars = len(gene_info["VARS"].split("\t"))
-                # write the annotation line with 'foo' repeated for each variant
-                anno_line = f'{gene_info["ENST"]}\tTranscript\t' + "\t".join(["Mask genes"] * length_of_vars) + "\n"
-                output_setfile_SAIGE.write(anno_line)
+                output_setfile_SAIGE.write(f'{gene_info["ENST"]}\t{gene_info["VARS"]}\n')
+
         return output_group_file
