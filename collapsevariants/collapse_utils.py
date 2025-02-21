d6072640c6aa86d10a473f152570aec2d3b02291
Alish Palmos <alish.palmos@insmed.com>
2025-02-21 15:49:46 +0000

    fix logger

--- collapsevariants/collapse_utils.py
+++ collapsevariants/collapse_utils.py
@@ -64,12 +64,14 @@ def generate_csr_matrix_from_bgen(variant_list: pd.DataFrame, bgen_path: Path, s
 
         # iterate through each gene in our search list
         for gene_n, current_gene in enumerate(search_list.itertuples()):
-            LOGGER.info('gene number is:', gene_n)
+            LOGGER.info(f'gene number is: {gene_n}')
 
             # implement a fix to ensure we are pulling out chromosomes as integers
             chrom = current_gene.CHROM
-            if 'chr' in chrom:
+            if isinstance(chrom, str):
                 chrom = chrom.replace("chr", "").strip()
+                if chrom not in ['X', 'Y']:
+                    chrom = int(chrom)
 
             # get the actual data from the bgen file
             variants = bgen_reader.fetch(chrom, current_gene.MIN, current_gene.MAX)
@@ -79,7 +81,7 @@ def generate_csr_matrix_from_bgen(variant_list: pd.DataFrame, bgen_path: Path, s
 
             # collect genotype arrays for each variant
             for current_variant in variants:
-                LOGGER.info('current variant is:', current_variant)
+                LOGGER.info(f'current variant is: {current_variant}')
 
                 if current_variant.rsid in current_gene.VARS:
                     # pull out the actual genotypes
