cf5f9f9f257f5cc7bcc19fbf15dd194fb58bd4b8
Alish Palmos <alish.palmos@insmed.com>
2025-03-07 12:47:56 +0000

    print

--- collapsevariants/parallelization_wrappers.py
+++ collapsevariants/parallelization_wrappers.py
@@ -49,6 +49,7 @@ def generate_genotype_matrices(genes: Dict[str, pd.DataFrame], bgen_index: Dict[
                                   chrom_bgen_index=bgen_index[bgen_prefix],
                                   variant_list=genes[bgen_prefix])
     genotype_index = {bgen_prefix: geno_matrix for bgen_prefix, geno_matrix in thread_utility}
+    print(genotype_index)
 
     return genotype_index
 
